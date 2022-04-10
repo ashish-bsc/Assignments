7a8c7e76c604f7107ab74e4a826dc03a3dd8c65a
Ashish Shrivastav <55585661+ashish-bsc@users.noreply.github.com>
2022-04-10 07:52:56 +0530

    Delete collect_productdetails.py

--- collect_productdetails.py
+++
@@ -1,7 +0,0 @@
-import scrapper as _scrapper
-
-def makejsonFile(prod : str):
-   product_details =  _scrapper.product_searched(prod)
-   with open("productdetails.json", "w") as outfile:
-        outfile.write(product_details)
-
